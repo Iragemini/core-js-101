3c02b0b21d0c73a3934b59c92057f6e7de84bfc4
Iragemini <iragemini@gmail.com>
2020-12-14 17:40:28 +0300

    feat: add solitions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -350,8 +350,17 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return arr.sort((a, b) => {
+    if (digits.indexOf(a) < digits.indexOf(b)) {
+      return -1;
+    }
+    if (digits.indexOf(a) > digits.indexOf(b)) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
 /**
@@ -446,8 +455,22 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.city > b.city) {
+      return 1;
+    }
+    if (a.city < b.city) {
+      return -1;
+    }
+    return 0;
+  });
 }
 
 /**
